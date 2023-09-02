8d6b74d455ab1b897146e5bf01ec47fde5479811
Ajith Redrigo <91521674+ajithredrigo2@users.noreply.github.com>
2023-09-02 14:32:56 +0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello, Docker Compose!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
